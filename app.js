fe81e7a9f623c018f511906d7e0e7f7cf01bc316
iamshaveenvf <shaveenfernando1@gmail.com>
2024-01-12 19:41:02 +0530

    campgrounds endpoint

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express')
-
 const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'views'))
+
+//mongoose
+mongoose.connect('mongodb+srv://shaveenleousj:pLfo7hKZycWUMIai@cluster0.ggsknbj.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error"))
+db.once("open",()=>{
+    console.log("Database Connected")
+})
 
 app.get('/',(req,res)=>{
-    res.send("Hello from yelp camp")
+    res.render("home")
+})
+
+app.get('/campgrounds',async(req,res)=>{
+    const campgrounds = await Campground.find({})
+    res.render("campgrounds/index", {campgrounds})
 })
 
 app.listen(3000,()=>{
