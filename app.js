dc5695de89389d6991737a64d8b994372388348d
iamshaveenvf <shaveenfernando1@gmail.com>
2024-01-29 10:56:13 +0530

    login signup

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
+const ExpressError = require('./utils/ExpressError')
+const session = require('express-session')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+const flash = require('connect-flash')
+const passport  = require('passport')
+const LocalStrategy  = require('passport-local')
+const User = require('./models/userModel')
+const userRoutes = require('./routes/users')
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname,'views'))
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
 
 //mongoose
 mongoose.connect('mongodb+srv://shaveenleousj:pLfo7hKZycWUMIai@cluster0.ggsknbj.mongodb.net/?retryWrites=true&w=majority',{
@@ -19,54 +21,66 @@ mongoose.connect('mongodb+srv://shaveenleousj:pLfo7hKZycWUMIai@cluster0.ggsknbj.
     useUnifiedTopology: true
 })
 
+
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error"))
 db.once("open",()=>{
     console.log("Database Connected")
 })
 
+
+//middleware
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'views'))
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+const sessionConfig = {
+    secret:'thisshouldbethesecret!',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly:true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge:1000*60*60*24*7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+// app.use(flash())
+
+// app.use((req,res,next)=>{
+//     res.locals.success = req.flash('success');
+//     res.locals.error= req.flash('error')
+//     next()
+// })
+
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
 app.get('/',(req,res)=>{
     res.render("home")
 })
 
-app.get('/campgrounds',async(req,res)=>{
-    const campgrounds = await Campground.find({})
-    res.render("campgrounds/index", {campgrounds})
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page Not Found',404));
 })
 
-app.get('/campgrounds/new',(req,res)=>{
-    res.render("campgrounds/new")
+//error handler middleware
+app.use((err,req,res,next)=>{
+    const {statusCode=500,message='Something went wrong'}=err;
+    res.status(statusCode).render('error',{err});
+
 })
-
-app.post('/campgrounds',async(req,res)=>{
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.get('/campgrounds/:id',async(req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    res.render("campgrounds/show", {campground})
-})
-
-app.get('/campgrounds/:id/edit', async(req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground})
-})
-
-app.put('/campgrounds/:id',async(req,res)=>{
-    const { id } = req.params
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.delete('/campgrounds/:id',async(req,res)=>{
-    const { id } = req.params
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-})
-
-
 
 app.listen(3000,()=>{
     console.log("App listening on port 3000")
